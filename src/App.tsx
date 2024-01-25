634718a660ea0b96216825e8e2b2d58878fa8ba6
yamageji <koyama.shigehito@ceroan.co.jp>
2024-01-25 15:05:03 +0900

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div className="flex gap-4 p-10">
+      <h1 className="color-blue-500 font-bold text-lg">Biome + UnoCSS</h1>
+      <p>test</p>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/main.tsx
@@ -0,0 +1,12 @@
+import "@unocss/reset/tailwind-compat.css";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "virtual:uno.css";
+
+// biome-ignore lint/style/noNonNullAssertion: <explanation>
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+);
